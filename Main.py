9fc02d4a0b4607d650ff01a223ca4dced0c54719
Abigail Pitcairn <abigail.pitcairn@maine.edu>
2024-12-03 15:32:46 -0500

    Updated pipeline

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import Evaluation
+import FileProcess
 import InvertedIndex
-import BM25Search
-import QueryProcess
+import SearchIndex
 import Rerank
 import time
 import sys
-
+# Author: Abigail Pitcairn
+# Version: Dec. 2, 2024
 
 # Main function to execute the search for multiple queries and save results
 def main(answers, topics1):
@@ -17,57 +18,57 @@ def main(answers, topics1):
         answers (str): Path to the JSON file containing the documents.
         topics1 (str): Path to the JSON file containing the queries.
     """
-    # Load the documents and build the inverted index
-    documents = QueryProcess.load_json_file(answers)
-    queries1 = QueryProcess.load_json_file(topics1)
-
-    print("Building inverted index...")
-    index = InvertedIndex.build_inverted_index(documents)
-
-    # Output result file names
+    # Load queries and documents
+    documents = FileProcess.load_json_file(answers)
+    queries1 = FileProcess.load_json_file(topics1)
+    # Name result files
     result1_file_name = 'result_topics1.tsv'
 
+    # Build inverted index
+    build_start_time = time.time()
+    print("Building inverted index...")
+    index = InvertedIndex.build_inverted_index_bm25(documents)
+    build_end_time = time.time()
+    print(f"Build time: {build_end_time-build_start_time:.2f} seconds.")
+
     # Perform BM25 search
-    print("Searching with BM25...")
+    print("Searching...")
     search_start = time.time()
-
-    search_result1 = BM25Search.search_index_by_query(queries1, index)
-
-    QueryProcess.save_to_result_file(search_result1, 'bm25_result.tsv')
-    Evaluation.evaluate_search_result('qrel_1.tsv', 'bm25_result.tsv')
+    search_result1 = SearchIndex.search_index_by_query(queries1, index)
     search_end = time.time()
-    print(f"BM25 Search completed in {search_end - search_start:.2f} seconds.")
-
-    # Perform reranking with BERT
-    print("Reranking search results with BERT...")
-    rerank_start = time.time()
-    reranked_result1 = Rerank.rerank(search_result1, queries1, documents)
-    rerank_end = time.time()
-    print(f"Reranking completed in {rerank_end - rerank_start:.2f} seconds.")
-
-    # Save results to the output file
-    print(f"Saving reranked results to {result1_file_name}...")
-    QueryProcess.save_to_result_file(reranked_result1, result1_file_name)
-
-    # Evaluate the reranked results
-    print("Evaluating reranked results...")
+    print(f"Search completed in {search_end - search_start:.2f} seconds.")
+    # Save and evaluate results
+    FileProcess.save_to_result_file(search_result1, result1_file_name)
     Evaluation.evaluate_search_result('qrel_1.tsv', result1_file_name)
 
+    # # Perform reranking with BERT
+    # print("Reranking search results with BERT...")
+    # rerank_start = time.time()
+    # reranked_result1 = Rerank.rerank(search_result1, queries1, documents)
+    # rerank_end = time.time()
+    # print(f"Reranking completed in {rerank_end - rerank_start:.2f} seconds.")
+    # # Save results to the output file
+    # print(f"Saving reranked results to {result1_file_name}...")
+    # FileProcess.save_to_result_file(reranked_result1, result1_file_name)
+    # # Evaluate the reranked results
+    # print("Evaluating reranked results...")
+    # Evaluation.evaluate_search_result('qrel_1.tsv', result1_file_name)
 
-# Terminal Command: python Main.py Answers.json topics_1.json topics_2.json
+
+# # Terminal Command: python Main.py Answers.json topics_1.json topics_2.json
 # if __name__ == "__main__":
-    # # Ensure two arguments are passed (answers.json and topics.json)
-    # if len(sys.argv) != 4:
-    #     print("Usage: python main.py <answers.json> <topics_1.json> <topics_2.json")
-    #     sys.exit(1)
-    #
-    # # Get file paths from command line arguments
-    # answers_file = sys.argv[1]
-    # topics1_file = sys.argv[2]
-    # topics2_file = sys.argv[3]
-    #
-    # # Call the main function with the file paths
-    # main(answers_file, topics1_file, topics2_file)
+#     # Ensure two arguments are passed (answers.json and topics.json)
+#     if len(sys.argv) != 4:
+#         print("Usage: python main.py <answers.json> <topics_1.json> <topics_2.json")
+#         sys.exit(1)
+#
+#     # Get file paths from command line arguments
+#     answers_file = sys.argv[1]
+#     topics1_file = sys.argv[2]
+#     topics2_file = sys.argv[3]
+#
+#     # Call the main function with the file paths
+#     main(answers_file, topics1_file, topics2_file)
 
 # Manual run code (remember to comment out the above block)
 main("Answers.json", "topics_1.json")
